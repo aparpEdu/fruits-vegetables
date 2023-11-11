97c054899639c9190892bce0e542410911634a18
moldovexc <alexanderparpulansky@gmail.com>
2023-11-11 16:10:03 +0200

    Adding fruit loader for loading hardcoded fruits

--- app/src/main/java/com/example/fruits_and_veggies/fruit/FruitSource.java
+++ app/src/main/java/com/example/fruits_and_veggies/fruit/FruitSource.java
@@ -1,5 +1,22 @@
-////package com.example.fruits_and_veggies.fruit;
-//
-//public class FruitSource {
-//    public static
-//}
+package com.example.fruits_and_veggies.fruit;
+
+import java.util.List;
+
+public class FruitSource {
+    private static FruitSource instance = null;
+    private final List<Fruit> fruits;
+
+    private FruitSource() {
+        this.fruits = FruitLoader.loadFruits();
+    }
+
+    public static FruitSource getInstance(){
+        if(instance == null){
+            instance = new FruitSource();
+        }
+        return instance;
+    }
+    public List<Fruit> getFruits() {
+        return fruits;
+    }
+}
