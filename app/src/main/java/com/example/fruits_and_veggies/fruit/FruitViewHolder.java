e4635e3b80946d5710cc22bd1f560f3b67c14565
moldovexc <alexanderparpulansky@gmail.com>
2023-11-23 16:17:35 +0200

    Adding viewholders for binding values in the recyclerview

--- app/src/main/java/com/example/fruits_and_veggies/fruit/FruitViewHolder.java
+++ app/src/main/java/com/example/fruits_and_veggies/fruit/FruitViewHolder.java
@@ -1,20 +1,40 @@
 package com.example.fruits_and_veggies.fruit;
 
+
+
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.fruits_and_veggies.R;
 import com.example.fruits_and_veggies.item.OnItemClickListener;
 
 public class FruitViewHolder extends RecyclerView.ViewHolder {
-    public FruitViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
-        super(itemView);
 
-        itemView.setOnClickListener(view -> {
-            if (listener != null){
-                listener.onItemClick(getBindingAdapterPosition());
-            }
-        });
+    private final TextView nameView;
+    private final TextView priceView;
+    private final TextView quantityView;
+    private final TextView sweetnessView;
+    public FruitViewHolder(@NonNull View itemView) {
+        super(itemView);
+        this.nameView = itemView.findViewById(R.id.fruit_name);
+        this.priceView = itemView.findViewById(R.id.fruit_price);
+        this.quantityView = itemView.findViewById(R.id.fruit_quantity);
+        this.sweetnessView = itemView.findViewById(R.id.fruit_sweetness);
+    }
+
+    public void bind(Fruit fruit) {
+        Log.d("FruitViewHolder", "bind - Fruit: " + fruit.getName());
+        String priceText = "Price: " + fruit.getPrice();
+        String quantityText = "Quantity: " + fruit.getQuantity();
+        String sweetnessText = "Sweetness: "  + fruit.getSweetIndex();
+        nameView.setText(fruit.getName());
+        priceView.setText(priceText);
+        quantityView.setText(quantityText);
+        sweetnessView.setText(sweetnessText);
+
     }
 }
--- app/src/main/java/com/example/fruits_and_veggies/veggie/VegetableViewHolder.java
+++ app/src/main/java/com/example/fruits_and_veggies/veggie/VegetableViewHolder.java
@@ -1,20 +1,34 @@
 package com.example.fruits_and_veggies.veggie;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.fruits_and_veggies.R;
 import com.example.fruits_and_veggies.item.OnItemClickListener;
 
 public class VegetableViewHolder extends RecyclerView.ViewHolder {
-    public VegetableViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
-        super(itemView);
+    private final TextView nameView;
+    private final TextView priceView;
+    private final TextView quantityView;
 
-        itemView.setOnClickListener(view -> {
-            if (listener != null){
-                listener.onItemClick(getBindingAdapterPosition());
-            }
-        });
+    public VegetableViewHolder(@NonNull View itemView) {
+        super(itemView);
+        this.nameView = itemView.findViewById(R.id.vegetable_name);
+        this.priceView = itemView.findViewById(R.id.vegetable_price);
+        this.quantityView = itemView.findViewById(R.id.vegetable_quantity);
+
+    }
+
+    public void bind(Vegetable vegetable) {
+        Log.d("VegetableViewHolder", "bind - Vegetable: " + vegetable.getName());
+        String priceText = "Price: " + vegetable.getPrice();
+        String quantityText = "Quantity: " + vegetable.getQuantity();
+        nameView.setText(vegetable.getName());
+        priceView.setText(priceText);
+        quantityView.setText(quantityText);
     }
 }
