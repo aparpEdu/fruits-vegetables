07dedd469e10f5e31918b85b43039af42fb05bd0
moldovexc <alexanderparpulansky@gmail.com>
2023-11-23 16:19:50 +0200

    Adding an adapter class for items

--- app/src/main/java/com/example/fruits_and_veggies/item/ItemAdapter.java
+++ app/src/main/java/com/example/fruits_and_veggies/item/ItemAdapter.java
@@ -1,24 +1,70 @@
 package com.example.fruits_and_veggies.item;
 
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.fruits_and_veggies.R;
+import com.example.fruits_and_veggies.fruit.Fruit;
+import com.example.fruits_and_veggies.fruit.FruitViewHolder;
+import com.example.fruits_and_veggies.veggie.Vegetable;
+import com.example.fruits_and_veggies.veggie.VegetableViewHolder;
+
+import java.util.List;
+
 public class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final List<Item> items;
+    private final OnItemClickListener listener;
+
+    public ItemAdapter(List<Item> items, OnItemClickListener listener) {
+        this.items = items;
+        this.listener = listener;
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Log.d("ItemAdapter", "onCreateViewHolder - viewType: " + viewType);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        if (viewType == 0) {
+            View view = inflater.inflate(R.layout.item_fruit, parent, false);
+            return new FruitViewHolder(view);
+        }
+            View view = inflater.inflate(R.layout.item_vegetable, parent, false);
+            return new VegetableViewHolder(view);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (items.get(position) instanceof Fruit){
+            return 0;
+        }
+        return 1;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+        holder.itemView.setOnClickListener(view -> {
+            if (listener != null){
+                listener.onItemClick(position);
+            }
+        });
+        Log.d("ItemAdapter", "onBindViewHolder - viewType: " + holder.getItemViewType() + ", position: " + position);
+        if (holder instanceof FruitViewHolder) {
+            ((FruitViewHolder) holder).bind((Fruit) items.get(position));
+        } else if (holder instanceof VegetableViewHolder) {
+            ((VegetableViewHolder) holder).bind((Vegetable) items.get(position));
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
+
 }
